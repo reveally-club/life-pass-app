1bf8102d28a6710b7f6a4ef7bda43966b67ea054
dilrong <dilrong@dilrong.com>
2023-03-06 23:55:42 +0900

    feat: 리더보드

--- src/pages/compoents/LeaderBoard.tsx
+++ src/pages/compoents/LeaderBoard.tsx
@@ -1,10 +1,11 @@
 import { collection } from "firebase/firestore";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { fireStore } from "../../../modules/firebase";
+import dayjs from "dayjs";
 
 export default function LeaderBoard() {
   const [value, loading, error] = useCollection(
-    collection(fireStore, "campaigns"),
+    collection(fireStore, "wakeup"),
     {
       snapshotListenOptions: { includeMetadataChanges: true },
     }
@@ -14,9 +15,22 @@ export default function LeaderBoard() {
     <div className="flex flex-col w-full items-center">
       <h2 className="text-4xl">리더보드</h2>
       <h3 className="w-full font-bold text-center mt-4 text-2xl italic text-transparent bg-clip-text bg-gradient-to-r from-sky-400 to-violet-400"></h3>
-      {/* {value?.docs.map((doc, key) => (
-        <div></div>
-      ))} */}
+      <ul className="w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg">
+        {value?.docs.map((doc, key) => (
+          <li
+            key={key}
+            className="w-full flex justify-between px-4 py-2 border-b border-gray-200 rounded-t-lg"
+          >
+            {/* <img src={doc.data().image} alt={doc.data().name} /> */}
+            <h6 className="text-base font-semibold">{doc.data().name}</h6>
+            <p>
+              {dayjs(doc.data().updatedAt.toDate()).format(
+                "YYYY-MM-DD HH:mm:ss"
+              )}
+            </p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
