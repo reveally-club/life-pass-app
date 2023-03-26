f8cdd21837ab89f0f88b244a8811bcb99360904e
dilrong <dilrong@dilrong.com>
2023-03-26 17:50:05 +0900

    feat: 갓생.기록 탭 추가

--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -4,10 +4,12 @@ import { fireStore } from "@/modules/firebase";
 import Layout from "../common/Layout";
 import HistoryCard from "./components/HistoryCard";
 import Loading from "../common/Loading";
+import { useState } from "react";
 
 export default function History() {
+  const [season, setSeason] = useState("season-test");
   const [value, loading] = useCollection(
-    query(collection(fireStore, "season-test"), orderBy("createdAt", "desc")),
+    query(collection(fireStore, season), orderBy("createdAt", "desc")),
     {
       snapshotListenOptions: { includeMetadataChanges: true },
     }
@@ -20,6 +22,16 @@ export default function History() {
         <h3 className="mt-2 text-transparent font-semibold bg-clip-text bg-gradient-to-r from-sky-400 to-violet-400">
           🔥 갓생을 나누고 같이 성장해요 🔥
         </h3>
+        <div className="flex flex-wrap gap-4">
+          <button
+            onClick={() => {
+              setSeason("season-test");
+            }}
+            className="relative mt-2 flex bg-gradient-to-r from-sky-200 to-violet-200 hover:from-sky-300 hover:to-violet-300 rounded-full px-3 py-1 text-sm mr-2 mb-2 hover:cursor-pointer"
+          >
+            season-test
+          </button>
+        </div>
         <div className="flex flex-wrap gap-8 justify-center mt-8">
           {loading ? <Loading /> : <div />}
           {value?.docs.map((doc, key) => (
